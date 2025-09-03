13e551ec22289b44d4a97b32050a465a3091de28
rahul-adepu <rahuladepu002@gmail.com>
2025-09-03 22:34:09 +0530

    Connect DB

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const connectDB = require("./db");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
+  await connectDB;
+  console.log("MongoDB connected");
   console.log("Server is running on port 3000");
 });
